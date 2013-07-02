984d27cfec94f05f32c0c3ef32dbddc8ff1453fa
Ajmal Afif <ajmalafif@gmail.com>
2013-07-02 20:54:54 +0800

    code kata'ing

--- roman_numberal_converter_spec.rb
+++ roman_numberal_converter_spec.rb
@@ -3,4 +3,14 @@ require 'roman_numeral_converter'
 
 describe RomanNumeralConverter do 
   
+Given(:converter) {RomanNumeralConverter.new }
+
+    Then { expect(converter.convert(1)).to eq 'I' }
+    Then { expect(converter.convert(2)).to eq 'II' }
+    Then { expect(converter.convert(5)).to eq 'V' }
+    Then { expect(converter.convert(6)).to eq 'VI' }
+    Then { expect(converter.convert(7)).to eq 'VII' }
+    Then { expect(converter.convert(10)).to eq 'X' }
+    Then { expect(converter.convert(11)).to eq 'XI' }
+    Then { pending; expect(converter.convert(15)).to eq 'XV' }
 end
--- roman_numeral_converter.rb
+++ roman_numeral_converter.rb
@@ -1,2 +1,14 @@
 class RomanNumeralConverter
+
+    def convert(number)
+        
+        if number >= 10
+            'X' + 'I' * (number % 10)
+        elsif number >= 5
+            'V' + 'I' * (number % 5)
+        else
+            'I' * number
+        end
+    end
+
 end
